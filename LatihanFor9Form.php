fadd9ec5da91e916f4b040efc735660df5612a45
Agusnindo <agusnindo@gmail.com>
2021-03-18 14:52:43 +0700

    Latihan Penerapan percabangan switch di BLK

--- LatihanFor9Form.php
+++ LatihanFor9Form.php
@@ -29,7 +29,7 @@
 	</nav>
 
 	<br>
-
+<!-- 
 	<Select name="urutan">
 		<?php for($a=1; $a<=10; $a++): ?>
 			<option value="<?php echo "$a" ?>">
@@ -46,7 +46,7 @@
 			</option>
 		<?php endfor ?>
 	</select>
-
+ -->
 	<br>
 	<br>
 	
@@ -63,45 +63,45 @@
 			echo "<br>";
 		}
 
-		for($a=1; $a<=$_GET['putaran']; $a++)
-		{ 
-			for ($i=1; $i<=$a; $i++) 
-			{ 
-				if($a==3){
-					if($i != 2){
-						echo $a;
-					}else{
-						echo "x";
-					}
-				}else{
-					echo $a;
-				}
-				// if ($a == 1) 
-				// {
-				// 	echo $a;
-				// }
-				// else if ($a == 2) 
-				// {
-				// 	echo $a;
-				// }
-				// else if ($a == 3) 
-				// {
-				// 	if($i != 2){
-				// 		echo $a;
-				// 	}else{
-				// 		echo "x";
-				// 	}
-				// }
-				// else if ($a == 4) 
-				// {
-				// 	echo $a;
-				// }
-				// else{
-				// 	echo $a;
-				// }
-			}
-			echo "<br>";
-		} 
+		// for($a=1; $a<=$_GET['putaran']; $a++)
+		// { 
+		// 	for ($i=1; $i<=$a; $i++) 
+		// 	{ 
+		// 		if($a==3){
+		// 			if($i != 2){
+		// 				echo $a;
+		// 			}else{
+		// 				echo "x";
+		// 			}
+		// 		}else{
+		// 			echo $a;
+		// 		}
+		// 		// if ($a == 1) 
+		// 		// {
+		// 		// 	echo $a;
+		// 		// }
+		// 		// else if ($a == 2) 
+		// 		// {
+		// 		// 	echo $a;
+		// 		// }
+		// 		// else if ($a == 3) 
+		// 		// {
+		// 		// 	if($i != 2){
+		// 		// 		echo $a;
+		// 		// 	}else{
+		// 		// 		echo "x";
+		// 		// 	}
+		// 		// }
+		// 		// else if ($a == 4) 
+		// 		// {
+		// 		// 	echo $a;
+		// 		// }
+		// 		// else{
+		// 		// 	echo $a;
+		// 		// }
+		// 	}
+		// 	echo "<br>";
+		// } 
 	}
 	?>
 
---
+++ LatihanSwitch10.php
@@ -0,0 +1,37 @@
+<?php 
+
+    $day = date('D');
+    // echo $day; 
+    switch ($day) {
+        case 'Sun':
+           $hari = "Minggu";
+            break;
+        
+        case 'Mon':
+            $hari = "Senin";
+            break;
+
+        case 'Tue':
+            $hari = "Selasa";
+            break;
+
+        case 'Wen':
+            $hari = "Rabu";
+            break; 
+
+        case 'Thu':
+            $hari = "Kamis";
+            break;  
+
+        case 'Fri':
+            $hari = "Jumat";
+            break;
+
+        default:
+           $hari = "Sabtu";
+            break;
+    }
+
+    echo $hari;
+
+?>
